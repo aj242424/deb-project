b005b2064ed287b217e623e647c85386cd6d6701
ALD LAB <ald.lab.aldrins6@gmail.com>
2023-07-25 10:45:11 +0530

    all code

---
+++ ui/src/components/Greetings.js
@@ -0,0 +1,10 @@
+export default function Greetings() {
+ return (
+ <div>
+<p class="font-bold text-2xl py-4" id = "greetingTxt">Hello world..!!</p>
+    <button class="bg-blue-500 text-white font bold py-4 px-6 rounded" onclick="changeGreeting()">ChangeGreetings</button>
+ </div>
+
+ );
+
+}
---
+++ ui/src/components/Visitors.js
@@ -0,0 +1,8 @@
+export default function Visitor() {
+
+    return (
+<p class="font-semibold text-lg py-4" id = "visitorCountTxt">Visitor Count: 78</p>
+    );
+
+
+}
